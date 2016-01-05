c1c770aba27aada82e498d77bf4f1358a759f07a
Armani Saldana & Steven Broderick <pair+manisaguana.stevula@devbootcamp.com>
2016-01-05 10:49:07 -0800

    Initial commit. Set up rails app and HTML/CSS/JS files

---
+++ js/miniQuery.js
@@ -0,0 +1,83 @@
+var miniQuery = (function(selector) {
+    var selected = document.querySelectorAll(selector);
+
+    return {
+        // DOM Manipulation
+        hide: function() {
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].style.visibility = "hidden";
+                }
+            }
+        },
+        show: function() {
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].style.visibility = "visible";
+                }
+            }
+        },
+        addClass: function(className) {
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].classList.add(className);
+                }
+            }
+        },
+        removeClass: function(className) {
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].classList.remove(className);
+                }
+            }
+        },
+
+        // Event Dispatching
+        on: function(eventName, eventHandler) {
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].addEventListener(eventName, eventHandler);
+                }
+            }
+        },
+        trigger: function(eventName) {
+            var e = new Event(eventName)
+            for(var i in selected) {
+                if(selected.hasOwnProperty(i)) {
+                    selected[i].dispatchEvent(e);
+                }
+            }
+        },
+
+        // AJAX Requests
+        request: function(type, url) {
+
+            var promise = new Promise(function(resolve, reject) {
+                var req = new XMLHttpRequest();
+                req.open(type, url, true);
+
+                req.onload = function() {
+                    if(req.status >= 200 && req.status < 400) {
+                        // success
+                        resolve(req.response);
+                    }
+                    else {
+                        // error
+                        reject(req.response);
+                    }
+                }
+                req.send("hi");
+            })
+
+            promise.then(function(resp) {
+                console.log("success");
+            }, function() {
+                console.log("failure");
+            });
+        }
+    }
+})
+
+var _$ = function(selector) {
+    return miniQuery(selector);
+}
