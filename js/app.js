f5cf88f9b755b72512c8dd2b6731e12ef358ace2
Armani Saldana & Steven Broderick <pair+manisaguana.stevula@devbootcamp.com>
2016-01-05 16:08:19 -0800

    List students on page through handlebar.js loop

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
-var students;
-var resp = _$().request("get", "http://localhost:3000/students")
-            .then(function(resp) {
-                students = JSON.parse(resp);
-            },
-            function(err) {
-                alert(err);
-            });
+// bind body tag
+var body = document.getElementsByTagName("body")[0];
 
-console.log(students)
-var x = "x"
+// bind template script
+var templateScript = document.getElementById("students-template").innerHTML;
+
+// compile template
+var template = Handlebars.compile(templateScript);
+
+
+// populate copperheads objects with data from server
+var promise = _$().request("get", "http://localhost:3000/students").then(
+    function(data) {
+      // set copperheads object
+      var copperheads = {students: JSON.parse(data)};
+
+      // add compiled html to page
+      body.innerHTML = template(copperheads);
+    },
+    function(err) {
+      alert(err);
+    }
+  );
+
+// var x = "x"
+
+
